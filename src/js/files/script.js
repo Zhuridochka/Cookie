a1f82da9fc2b4a4f68cfc63cc9553d1b0c85c613
Zhuridochka <zhuridochka@gmail.com>
2024-10-02 11:58:03 +0300

    Add Collection section

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -2,3 +2,37 @@
 import { isMobile } from "./functions.js";
 // Підключення списку активних модулів
 import { flsModules } from "./modules.js";
+
+const isTouchScreen = window.matchMedia("(any-hover:none)").matches;
+
+window.addEventListener("load", windowLoaded);
+
+function windowLoaded() {
+  // let keypressActions = (e) => {
+  //   if (e.key === "Escape") {
+  //     document.documentElement.classList.remove("catalog-open");
+  //   }
+  // };
+
+  //* Додавання классу .search-open до тегу <head>
+  let documentActions = (e) => {
+    const targetElement = e.target;
+    const typeEvent = e.type;
+    const targetTag = targetElement.tagName;
+  };
+
+  //* Функція для додавання від'ємного margin до блогу .presentation
+  function updateHeightObjectPaddingBlock() {
+    const presentation = document.querySelector(".presentation");
+    if (!presentation) return;
+
+    const presentationHeight = presentation.offsetHeight;
+    const halfPresentationHeight = presentationHeight / 2;
+    const hero = document.querySelectorAll(".hero");
+
+    if (hero.length > 0) {
+      presentation.style.marginBlockStart = `${-halfPresentationHeight}px`;
+    }
+  }
+  updateHeightObjectPaddingBlock();
+}
