cbc46d04206c08266881694f7df922cdf3de4443
Zhuridochka <zhuridochka@gmail.com>
2024-10-05 16:18:28 +0300

    Add and adaptive Jobs page

--- src/js/app.js
+++ src/js/app.js
@@ -45,7 +45,7 @@ flsFunctions.menuInit();
 Документація: https://template.fls.guru/template-docs/modul-spojlery.html
 Сніппет (HTML): spollers
 */
-// flsFunctions.spollers();
+flsFunctions.spollers();
 
 /*
 Модуль "Таби"
@@ -179,7 +179,7 @@ flsForms.formFieldsInit({
 Документація плагіна: https://github.com/Grsmto/simplebar/tree/master/packages/simplebar
 Сніппет(HTML): 
 */
-// import './files/scroll/simplebar.js';
+import "./files/scroll/simplebar.js";
 
 // Ліниве (відкладене) завантаження картинок
 // Документація по роботі у шаблоні: https://template.fls.guru/template-docs/modul-lenivaya-podgruzka-lazy-loading.html
